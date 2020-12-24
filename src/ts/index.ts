b2bd261866c319f68b0cbba3d8a32752812110ac
Ikrom Murodov <ikrommurodov2001@gmail.com>
2020-12-23 23:56:16 -0700

    Added the ability to use hooks

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -8,11 +8,13 @@ interface IHooks<T> {
   ownKeys?(target: T): void;
 }
 
-function proxyHandler<T extends object = object>(): ProxyHandler<T> {
+function proxyHandler<T extends object = object>(
+  hooks?: IHooks<T>,
+): ProxyHandler<T> {
   return {
     get(target, key): unknown {
       const result = Reflect.get(target, key);
-
+      if (result && hooks?.getHook) hooks.getHook(target, key);
       if (typeof result === 'function') return result.bind(target);
       if (typeof result === 'object') return new Proxy(result, this);
       return result;
@@ -20,25 +22,29 @@ function proxyHandler<T extends object = object>(): ProxyHandler<T> {
 
     set(target, key, value): boolean {
       const result = Reflect.set(target, key, value);
+      if (result && hooks?.setHook) hooks.setHook(target, key, value);
       return result;
     },
 
     deleteProperty(target, key): boolean {
       const result = Reflect.deleteProperty(target, key);
+      if (result && hooks?.deleteProperty) hooks.deleteProperty(target, key);
       return result;
     },
 
     has(target, key): boolean {
       const result = Reflect.has(target, key);
+      if (result && hooks?.has) hooks.has(target, key);
       return result;
     },
 
     ownKeys(target): (string | number | symbol)[] {
+      if (hooks?.ownKeys) hooks.ownKeys(target);
       return Reflect.ownKeys(target);
     },
   };
 }
 
-function reactive<T extends object = object>(target: T): T {
-  return new Proxy(target, proxyHandler());
+function reactive<T extends object = object>(target: T, hooks?: IHooks<T>): T {
+  return new Proxy(target, proxyHandler(hooks));
 }
